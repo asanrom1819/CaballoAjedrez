bb7949cfafcb25cd1ee4d748d95d93017aa628af
asanrom <asanrom@DESKTOP-OJSE09M.home>
2018-11-19 11:48:19 +0100

    Creación de la clase Posicion
    Se crea la clase Posición en el paquete:
    org.iesalandalus.programacion.caballoajedrez
    
    junto a sus dos atributos.

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -0,0 +1,17 @@
+/*
+ * Actividad 05
+ * Se crea la clase en el paquete indicado en el esquema de la tarea
+ * */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ * Esta clase determina y modifa la posición de la pieza del
+ * caballo en un tablero de ajedrez.
+ * @author asanrom
+ *
+ */
+public class Posicion {
+	/*Lista de Atributos*/
+	private int fila;
+	private char columna;
+}
