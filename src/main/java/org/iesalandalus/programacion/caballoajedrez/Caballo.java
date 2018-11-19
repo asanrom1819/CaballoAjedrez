0a80a9667ba3022e1d573499a42d43ae7cf46fac
asanrom <asanrom@DESKTOP-OJSE09M.home>
2018-11-19 19:14:17 +0100

    Se crean los métodos get y set de la clase Caballo.

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -2,6 +2,9 @@
 /*
  * Actividad 11
  * Se crea la clase Caballo en el paquete org.iesalandalus.programacion.caballoajedrez
+ * 
+ * Actividad 12
+ * Se crean los métodos get y set
  * */
 package org.iesalandalus.programacion.caballoajedrez;
 
@@ -10,4 +13,47 @@ public class Caballo {
 	private Color color;
 	private Posicion posicion;
 	
+	/*Métodos get y set*/
+	
+	/**
+	 * Determina la posición actual del caballo.
+	 * @param posicion El valor de la nueva posición.
+	 * @throws IllegalArgumentException Cuando se suministra un parámetro que apunte a null.
+	 */
+	private void setPosicion(Posicion posicion) throws IllegalArgumentException {
+		//Se valida el parámetro
+		if (posicion == null) throw new IllegalArgumentException("La instancia de Posicion apunta a null");
+		else {
+			this.posicion = posicion;
+		}
+	}
+	
+	/**
+	 * Obtiene la posición actual del caballo.
+	 * @return La posición actual del caballo.
+	 */
+	public Posicion getPosicion() {
+		return this.posicion;
+	}
+	
+	/**
+	 * Determina el color del caballo.
+	 * @param color El nuevo color para la pieza.
+	 * @throws IllegalArgumentException Cuando se pasa por parámetro un color que apunta a null.
+	 */
+	private void setColor(Color color) throws IllegalArgumentException {
+		//Se valida el parámetro para que éste no apunte a null.
+		if (color == null) throw new IllegalArgumentException("La instancia de Color apunta a null");
+		else {
+			this.color = color;
+		}
+	}
+	
+	/**
+	 * Obtiene el color actual del caballo.
+	 * @return El color actual del caballo.
+	 */
+	public Color getColor() {
+		return this.color;
+	}
 }
