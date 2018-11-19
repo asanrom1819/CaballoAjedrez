b58d918aef074686cac8f2e04dcc5d55b36d16ee
asanrom <asanrom@DESKTOP-OJSE09M.home>
2018-11-19 10:47:13 +0100

    Se crea el Enumerado Color en paquete: org.iesalandalus.programacion.caballoajedrez

---
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Color.java
@@ -0,0 +1,15 @@
+/*
+ * Actividad 03
+ * Se crea el Enumerado en el paquete indicado en el esquema de la tarea
+ * */
+package org.iesalandalus.programacion.caballoajedrez;
+
+/**
+ * Enumerado con los colores de las piezas de ajedrez disponibles.
+ * @author asanrom
+ *
+ */
+public enum Color {
+	//Se determinan los valores disponibles para el Enumerado.
+	BLANCO, NEGRO;
+}
